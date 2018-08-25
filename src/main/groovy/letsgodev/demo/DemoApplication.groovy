79f95711473d3f2a4c2fa1fb017ec9a19a16162c
Yasuharu Nakano <ynak@jggug.org>
2018-08-25 13:13:57 +0900

    Add Spock sample

--- src/main/groovy/letsgodev/demo/DemoApplication.groovy
+++ src/main/groovy/letsgodev/demo/DemoApplication.groovy
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class DemoApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run DemoApplication, args
-	}
+    static void main(String[] args) {
+        SpringApplication.run DemoApplication, args
+    }
 }
