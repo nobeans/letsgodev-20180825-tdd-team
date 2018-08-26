4d54b677f65d26997e9e1e2c7eed4148735a7fd6
Yasuharu Nakano <ynak@jggug.org>
2018-08-26 19:56:35 +0900

    [from now] 2018/08/26 19:56:35
     .../groovy/letsgodev/demo/RateUtilsSpec.groovy     | 22 +++++++++++++++++++++-
     1 file changed, 21 insertions(+), 1 deletion(-)

--- src/test/groovy/letsgodev/demo/RateUtilsSpec.groovy
+++ src/test/groovy/letsgodev/demo/RateUtilsSpec.groovy
@@ -2,9 +2,11 @@ package letsgodev.demo
 
 import spock.lang.Specification
 
+import java.time.LocalDate
+
 class RateUtilsSpec extends Specification {
 
-    void "金額に端数がある場合は小数第一位で四捨五入する"() {
+    void "金額に端数がある場合は小数点第一位で四捨五入する"() {
         expect:
         RateUtils.round(123.45) == 123
 
@@ -23,4 +25,22 @@ class RateUtilsSpec extends Specification {
         123.5    | 124
         123.9    | 124
     }
+
+    void "日割り計算する"() {
+        expect:
+        RateUtils.prorateDaily(cutofDate, contractDate, fullRate) == proratedRate
+
+        where:
+        cutofDate                 | contractDate              | fullRate | proratedRate
+        LocalDate.of(2018, 8, 31) | LocalDate.of(2018, 8, 1)  | 31.0     | 31.0
+        LocalDate.of(2018, 8, 31) | LocalDate.of(2018, 8, 2)  | 31.0     | 30.0
+        LocalDate.of(2018, 8, 31) | LocalDate.of(2018, 8, 30) | 31.0     | 2.0
+        LocalDate.of(2018, 8, 31) | LocalDate.of(2018, 8, 31) | 31.0     | 1.0
+        LocalDate.of(2018, 6, 30) | LocalDate.of(2018, 6, 1)  | 30.0     | 30.0
+        LocalDate.of(2018, 6, 30) | LocalDate.of(2018, 6, 2)  | 30.0     | 29.0
+        LocalDate.of(2018, 6, 30) | LocalDate.of(2018, 6, 29) | 30.0     | 2.0
+        LocalDate.of(2018, 6, 30) | LocalDate.of(2018, 6, 30) | 30.0     | 1.0
+        LocalDate.of(2018, 6, 30) | LocalDate.of(2018, 6, 15) | 100.0    | 53.0 // 端数は小数点第一位で四捨五入されること
+    }
 }
+
