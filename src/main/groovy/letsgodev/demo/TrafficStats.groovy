6b7396797578a9f6fc9fb2c725f55ad0c7ff0bf4
Yasuharu Nakano <ynak@jggug.org>
2018-08-26 14:24:01 +0900

    [from now] 2018/08/26 14:24:01
     src/main/groovy/letsgodev/demo/TrafficStats.groovy | 20 ++++++++++++++++++++
     1 file changed, 20 insertions(+)

--- src/main/groovy/letsgodev/demo/TrafficStats.groovy
+++ src/main/groovy/letsgodev/demo/TrafficStats.groovy
@@ -7,6 +7,26 @@ import jdk.nashorn.internal.ir.annotations.Immutable
 @Immutable
 class TrafficStats {
 
+    /** 合計電話通話時間(sec) */
     long totalCallSeconds
+
+    /** 国際電話通話時間(sec) */
+    long internationalCallSeconds
+
+    /**
+     * サービス利用通話時間(sec)。
+     * <p>
+     * 以下の有料電話サービスの利用時間の合計です。
+     * <ul>
+     *   <li>テレドーム(0180から始まる番号)、
+     *   <li>ナビダイヤル等(0570から始まる番号)
+     *   <li>番号案内(104)(0570から始まる番号)
+     *   <li>番号案内(104)
+     *   <li>等
+     * </ul>
+     */
+    long service0180CallSeconds
+
+    /** データ通信量(byte) */
     long totalDataBytes
 }
