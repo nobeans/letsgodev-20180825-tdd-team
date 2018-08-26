b447f7ab6f450716e78d85aa4394acaf2ec12cc7
Yasuharu Nakano <ynak@jggug.org>
2018-08-26 14:08:15 +0900

    [from now] 2018/08/26 14:08:15
     src/main/groovy/letsgodev/demo/CallPlan.groovy | 31 +++++++++++++++++---------
     1 file changed, 21 insertions(+), 10 deletions(-)

--- src/main/groovy/letsgodev/demo/CallPlan.groovy
+++ src/main/groovy/letsgodev/demo/CallPlan.groovy
@@ -5,15 +5,28 @@ import groovy.transform.TupleConstructor
 @TupleConstructor
 enum CallPlan implements Rateable {
 
-    BASIC_THE_NEXT("レッツゴーデベロッパー THE NEXT プラン", 4500),
+    BASIC_THE_NEXT("レッツゴーデベロッパー THE NEXT プラン"){
+        @Override
+        Integer rate(CustomerContract customerContract, TrafficStats trafficStats) {
+            4500
+        }
+    },
 
-    BASIC_HENSHIN("レッツゴーデベロッパー 変真 プラン", 3500),
+    BASIC_HENSHIN("レッツゴーデベロッパー 変真 プラン"){
+        @Override
+        Integer rate(CustomerContract customerContract, TrafficStats trafficStats) {
+            3500
+        }
+    },
 
-    BASIC_X("レッツゴーデベロッパー X プラン", 2500)
+    BASIC_X("レッツゴーデベロッパー X プラン"){
+        @Override
+        Integer rate(CustomerContract customerContract, TrafficStats trafficStats) {
+            2500
+        }
+    }
 
     String name
-
-    Integer rate
 
     @Override
     String toString() {
@@ -21,7 +34,5 @@ enum CallPlan implements Rateable {
     }
 
     @Override
-    Integer rate(CustomerContract customerContract, TrafficStats trafficStats) {
-        rate
-    }
+    abstract Integer rate(CustomerContract customerContract, TrafficStats trafficStats)
 }
